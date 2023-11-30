b1a235c9906dbec3fcebc90b40453b36d3609893
flo-schu <fluncki@protonmail.com>
2023-11-30 18:46:42 +0100

    add pymoo test and add fixtures for simulation init

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -3,32 +3,27 @@ import xarray as xr
 import numpy as np
 from click.testing import CliRunner
 
-from pymob.utils.store_file import prepare_casestudy
+from tests.fixtures import init_test_case_study
 
-def test_scripting_API():
-    config = prepare_casestudy(
-        case_study=("test_case_study", "test_scenario"),
-        config_file="settings.cfg"
-    )
-    from case_studies.test_case_study.sim import Simulation
-    
-    sim = Simulation(config=config)
-    sim.setup()
+
+def test_pyabc():
+    sim = init_test_case_study()
     sim.set_inferer(backend="pyabc")
     sim.inferer.run()
 
     # TODO: write test (something like if error smaller x)
 
 
+def test_pymoo():
+    sim = init_test_case_study()
+    sim.set_inferer(backend="pymoo")
+    sim.inferer.run()
+
+    # TODO: write test (something like if error smaller x)
+
+
 def test_inference_evaluation():
-    config = prepare_casestudy(
-        case_study=("test_case_study", "test_scenario"),
-        config_file="settings.cfg"
-    )
-    from case_studies.test_case_study.sim import Simulation
-    
-    sim = Simulation(config=config)
-    sim.setup()
+    sim = init_test_case_study()
     sim.set_inferer(backend="pyabc")
 
     sim.inferer.load_results()
@@ -42,13 +37,6 @@ def test_inference_evaluation():
     fig.savefig(sim.config.case_study.output_path + "/pyabc_posterior_predictions.png")
 
 
-def test_commandline_API_redis():
-    from pymob.infer import main
-    runner = CliRunner()
-    
-    args = "--case_study=test_case_study --scenario=test_scenario"
-    result = runner.invoke(main, args.split(" "))
-
 def test_commandline_API_infer():
     from pymob.infer import main
     runner = CliRunner()
@@ -59,3 +47,7 @@ def test_commandline_API_infer():
 
 # test_inference_evaluation()
 # test_commandline_API_infer()
+
+if __name__ == "__main__":
+    import sys; import os; sys.path.append(os.getcwd())
+    test_pymoo()
