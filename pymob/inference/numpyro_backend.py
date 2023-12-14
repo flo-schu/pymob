7f5e54075e40d511e14cfc3856e292f228118aec
flo-schu <fluncki@protonmail.com>
2023-12-14 08:56:10 +0100

    Implementing a mode that works

--- pymob/inference/numpyro_backend.py
+++ pymob/inference/numpyro_backend.py
@@ -14,7 +14,6 @@ from diffrax import (
     PIDController, 
     RecursiveCheckpointAdjoint
 )
-jax.config.update("jax_enable_x64", True)
             
 
 DISTRIBUTION_MAPPER = {
@@ -78,126 +77,138 @@ class NumpyroBackend:
             pri = numpyro.sample(p_n, dist.LogNormal(loc=p_p["scale"], scale=p_p["s"]))
             
             theta.update({p_n: pri})
-
-    def model(self, solver, obs):
-        # k_i = numpyro.sample("k_i", dist.LogNormal(loc=jnp.log(5), scale=2))
-        k_i = numpyro.sample("k_i", dist.Normal(loc=5, scale=10))
-        # r_rt = numpyro.sample("r_rt", dist.LogNormal(loc=jnp.log(0.1), scale=s))
-        # r_rd = numpyro.sample("r_rd", dist.LogNormal(loc=jnp.log(0.5), scale=s))
-        # v_rt = numpyro.sample("v_rt", dist.LogNormal(loc=jnp.log(1.0), scale=s))
-        # z_ci = numpyro.sample("z_ci", dist.LogNormal(loc=jnp.log(500.0), scale=s))
-        # r_pt = numpyro.sample("r_pt", dist.LogNormal(loc=jnp.log(0.1), scale=s))
-        # r_pd = numpyro.sample("r_pd", dist.LogNormal(loc=jnp.log(0.01), scale=s))
-        # volume_ratio = numpyro.sample("volume_ratio", dist.LogNormal(loc=jnp.log(5000), scale=s))
-        # z = numpyro.sample("z", dist.LogNormal(loc=jnp.log(2.0), scale=s))
-        # kk = numpyro.sample("kk", dist.LogNormal(loc=jnp.log(0.02), scale=s))
-        # b_base = numpyro.sample("b_base", dist.LogNormal(loc=jnp.log(0.1), scale=s))
-        # sigma_cint = numpyro.sample("sigma_cint", dist.LogNormal(loc=jnp.log(0.1), scale=s))
-        # sigma_cext = numpyro.sample("sigma_cext", dist.LogNormal(loc=jnp.log(0.1), scale=s))
-        # sigma_nrf2 = numpyro.sample("sigma_nrf2", dist.HalfNormal(scale=0.1))
-
-        theta = {
-            # "k_i": 1e17, # breaks the ODE
-            "k_i": k_i,
-            "r_rt": 0.1,
-            "r_rd": 0.5,
-            "v_rt": 1.0,
-            "z_ci": 500,
-            "r_pt": 0.1,
-            "r_pd": 0.01,
-            "volume_ratio": 5000,
-            "z": 2.0,
-            "kk": 0.02,
-            "b_base": 0.1,
-            # "r_rt": r_rt,
-            # "r_rd": r_rd,
-            # "v_rt": v_rt,
-            # "z_ci": z_ci,
-            # "r_pt": r_pt,
-            # "r_pd": r_pd,
-            # "volume_ratio": volume_ratio,
-            # "z": z,
-            # "kk": kk,
-            # "b_base": b_base,
-        }
-        # theta = (k_i, 0.1, 0.5, 1.0 ,500, 0.1, 0.01, 5000)
-
-        sim = solver(theta=theta)
-
-        # cext = numpyro.sample("cext", dist.LogNormal(loc=jnp.log(y[0]), scale=sigma_cext).mask(mask["cext"].values), obs=obs["cext"].values)
-        # cint = numpyro.sample("cint", dist.LogNormal(loc=jnp.log(y[1]), scale=sigma_cint).mask(mask["cint"].values), obs=obs["cint"].values)
-        
-        sim_cint_i = sim[1][0,:,0]
-        obs_cint_i = obs[1][0,:,0]
-        numpyro.sample("cint", dist.Normal(loc=sim_cint_i, scale=10), obs=obs_cint_i)
-    
-        # nrf2 = numpyro.sample("nrf2", dist.LogNormal(loc=jnp.log(y[2]), scale=sigma_nrf2).mask(mask["nrf2"].values), obs=obs["nrf2"].values)
-        # leth = numpyro.sample("lethality", dist.Binomial(probs=y[3], total_count=nzfe).mask(mask["lethality"].values), obs=obs["lethality"].values)
-
-
-        
+       
 
     def run(self):
-        from mod import tktd_rna_3
+        from mod import tktd_rna_3, mappar
+        # jax.config.update("jax_enable_x64", True)
 
-        # @jax.jit
-        # def solver(y0, params, meas_times):
-        #     """Solves the SIR model numerically
-
-        #     A total population of 1000 persons is assumed. The initial value of susceptible
-        #     persons is 1000 - init_i, whereas the initial number of infected persons is init_i.
-        #     The solution is returned at the given meas_times.
-
-        #     Args:
-        #         init_i: Initial value of infected persons (state I)
-        #         params: Array with parameters general death rate, cure/death rate,
-        #             general birth rate, infection rate (in this order)
-        #         meas_times: Measurement time points
-
-        #     Returns:
-        #         Array with solution at the measurement time points (shape is n_meas x 3)
-        #     """
-
-
-        #     f = lambda t, y0, params: tktd_rna_3(t, y0, **params)
-        
-        #     term = ODETerm(f)
-        #     solver = Kvaerno5()
-        #     saveat = SaveAt(ts=meas_times)
-        #     stepsize_controller = PIDController(rtol=1e-8, atol=1e-8)
-
-        #     sol = diffeqsolve(
-        #         term, solver, 
-        #         t0=24, t1=120.0, dt0=0.1, 
-        #         y0=y0, 
-        #         saveat=saveat,
-        #         stepsize_controller=stepsize_controller, 
-        #         args=params,
-        #         max_steps=10**6
-        #     )
-        #     return sol.ys
-
-        y_sim = self.evaluator(self.simulation.model_parameter_dict)
-        
         n_chains = 1
         numpyro.set_host_device_count(n_chains)
+
+
+        @jax.jit
+        def solver(theta, y0, t):
+            """Solves the SIR model numerically
+
+            A total population of 1000 persons is assumed. The initial value of susceptible
+            persons is 1000 - init_i, whereas the initial number of infected persons is init_i.
+            The solution is returned at the given meas_times.
+
+            Args:
+                init_i: Initial value of infected persons (state I)
+                params: Array with parameters general death rate, cure/death rate,
+                    general birth rate, infection rate (in this order)
+                meas_times: Measurement time points
+
+            Returns:
+                Array with solution at the measurement time points (shape is n_meas x 3)
+            """
+
+
+            args = mappar(tktd_rna_3, theta, exclude=["X", "t"]) 
+            f = lambda t, y0, params: tktd_rna_3(t, y0, *params)
+        
+            term = ODETerm(f)
+            solver = Dopri5()
+            saveat = SaveAt(ts=t)
+            stepsize_controller = PIDController(rtol=1e-6, atol=1e-7)
+
+            sol = diffeqsolve(
+                term, solver, 
+                t0=24, t1=120.0, dt0=0.1, 
+                y0=y0, 
+                saveat=saveat,
+                stepsize_controller=stepsize_controller, 
+                args=args,
+                max_steps=10**6
+            )
+            return sol.ys
+
+
+        def model(solver, obs):
+            k_i = numpyro.sample("k_i", dist.LogNormal(loc=jnp.log(5), scale=1))
+            # k_i = numpyro.sample("k_i", dist.Normal(loc=5, scale=10))
+            r_rt = numpyro.sample("r_rt", dist.LogNormal(loc=jnp.log(0.1), scale=1))
+            r_rd = numpyro.sample("r_rd", dist.LogNormal(loc=jnp.log(0.5), scale=1))
+            v_rt = numpyro.sample("v_rt", dist.LogNormal(loc=jnp.log(1.0), scale=1))
+            z_ci = numpyro.sample("z_ci", dist.LogNormal(loc=jnp.log(500.0), scale=1))
+            # r_pt = numpyro.sample("r_pt", dist.LogNormal(loc=jnp.log(0.1), scale=s))
+            # r_pd = numpyro.sample("r_pd", dist.LogNormal(loc=jnp.log(0.01), scale=s))
+            # volume_ratio = numpyro.sample("volume_ratio", dist.LogNormal(loc=jnp.log(5000), scale=s))
+            # z = numpyro.sample("z", dist.LogNormal(loc=jnp.log(2.0), scale=s))
+            # kk = numpyro.sample("kk", dist.LogNormal(loc=jnp.log(0.02), scale=s))
+            # b_base = numpyro.sample("b_base", dist.LogNormal(loc=jnp.log(0.1), scale=s))
+            # sigma_cint = numpyro.sample("sigma_cint", dist.LogNormal(loc=jnp.log(0.1), scale=s))
+            # sigma_cext = numpyro.sample("sigma_cext", dist.LogNormal(loc=jnp.log(0.1), scale=s))
+            # sigma_nrf2 = numpyro.sample("sigma_nrf2", dist.HalfNormal(scale=0.1))
+
+            theta = {
+                "k_i": k_i,
+                "r_rt": r_rt,
+                "r_rd": r_rd,
+                "v_rt": v_rt,
+                "z_ci": z_ci,
+                # "r_pt": r_pt,
+                # "r_pd": r_pd,
+                # "volume_ratio": volume_ratio,
+                # "z": z,
+                # "kk": kk,
+                # "b_base": b_base,
+                
+                # "k_i": 1e17, # breaks the ODE
+                # "r_rt": 0.1,
+                # "r_rd": 0.5,
+                # "v_rt": 1.0,
+                # "z_ci": 500,
+                "r_pt": 0.1,
+                "r_pd": 0.01,
+                "volume_ratio": 5000,
+                "z": 2.0,
+                "kk": 0.02,
+                "b_base": 0.1,
+            }
+            # theta = (k_i, 0.1, 0.5, 1.0 ,500, 0.1, 0.01, 5000)
+            sim = solver(theta=theta)
+
+            # cext = numpyro.sample("cext", dist.LogNormal(loc=jnp.log(y[0]), scale=sigma_cext).mask(mask["cext"].values), obs=obs["cext"].values)
+            # cint = numpyro.sample("cint", dist.LogNormal(loc=jnp.log(y[1]), scale=sigma_cint).mask(mask["cint"].values), obs=obs["cint"].values)
+            
+            sim_cint_i = sim[1]
+            obs_cint_i = obs[1]
+            numpyro.sample("cint", dist.Normal(loc=sim_cint_i, scale=10), obs=obs_cint_i)
+        
+            # nrf2 = numpyro.sample("nrf2", dist.LogNormal(loc=jnp.log(y[2]), scale=sigma_nrf2).mask(mask["nrf2"].values), obs=obs["nrf2"].values)
+            # leth = numpyro.sample("lethality", dist.Binomial(probs=y[3], total_count=nzfe).mask(mask["lethality"].values), obs=obs["lethality"].values)
+
+
+        theta = self.simulation.model_parameter_dict
+        theta.update({"volume_ratio": 5000})
+        y0 = (18.0, 0.0, 0.0, 0.0)
+        t = jnp.array(self.simulation.coordinates["time"])
+
+        ode_sol = partial(solver, y0=y0, t=t)
+
+        y_sim = ode_sol(theta=theta)
 
         # obs = self.observations.transpose("id", "time", "substance")
         # mask = obs.isnull()
         # n = len(self.simulation.coordinates["time"])
         # nzfe = jnp.tile(self.observations.nzfe.values, n).reshape((n, 50)).T
         
-        # model = partial(self.model, solver=solver, obs=obs, mask=mask, nzfe=nzfe)    
-        model = partial(self.model, solver=self.evaluator)    
+        model = partial(model, solver=ode_sol)    
+        # model = partial(self.model, solver=self.evaluator)    
         kernel = infer.NUTS(
             model, 
             dense_mass=False, 
             # inverse_mass_matrix=inverse_mass_matrix,
-            step_size=0.1,
+            # step_size=0.1,
             adapt_mass_matrix=True,
             adapt_step_size=True,
-            target_accept_prob=0.9,
-            init_strategy=infer.init_to_value(values={"k_i": 5.0})
+            target_accept_prob=0.8,
+            init_strategy=infer.init_to_value(
+                values={"k_i": 5.0, "r_rt": 0.1, "r_rd": 0.5, "v_rt": 1.0, "z_ci": 500}
+            )
         )
 
         # TODO: Try with init args
@@ -209,7 +220,7 @@ class NumpyroBackend:
             progress_bar=True,
         )
 
-        mcmc.run(jax.random.PRNGKey(123), obs=y_sim)
+        mcmc.run(jax.random.PRNGKey(5), obs=y_sim)
         mcmc.print_summary()
 
 
@@ -222,3 +233,5 @@ class NumpyroBackend:
         proposal = mcmc.last_state
 
         # apprently also last state can be passed to the run method
+        # TODO: It would possibly be a good idea to split warmup and sampling 
+        #       and save the mass matrix if warump was successful. 
