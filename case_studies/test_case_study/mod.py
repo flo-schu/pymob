2d228894f41c695fc603ef7ce617a51ffa2bcca1
flo-schu <fluncki@protonmail.com>
2023-12-18 16:20:18 +0100

    adapt mod.py and sim.py from test_case_study to breaking API changes

--- case_studies/test_case_study/mod.py
+++ case_studies/test_case_study/mod.py
@@ -1,3 +1,35 @@
+from pymob.sim.solvetools import mappar
+from scipy.integrate import odeint
+
+def solve(model, parameters, coordinates, foo):
+    """Initial value problems always need the same number of recurrent arguments
+
+    - parameters: define the model
+    - y0: set the initial values of the ODE states
+    - coordinates: are needed to know over which values to integrate
+    - seed: In case stochastic processes take place inside the model this is necessary
+    
+    In order to make things explicit, all information which is needed by the
+    model needs to be specified in the function signature. 
+    This also makes the solvers functionally oriented, a feature that helps the
+    usability of models accross inference frameworks. Where functions should not
+    have side effects.
+
+    Additionally, passing arguments via the signature makes it easier to write
+    up models in a casual way and only later embed them into more regulated
+    structures such as pymob
+
+    """
+    odeargs = mappar(model, parameters["parameters"], exclude=["t", "y"])
+
+    return odeint(
+        func=model,
+        y0=parameters["y0"],
+        t=coordinates["time"],
+        args=odeargs,
+    )
+
+
 def lotka_volterra(y, t, alpha, beta, gamma, delta):
     """
     Calculate the rate of change of prey and predator populations.
