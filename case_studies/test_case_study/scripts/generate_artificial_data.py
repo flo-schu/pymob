6a099204e25f566469359c384132623bf89ba5f8
flo-schu <fluncki@protonmail.com>
2024-01-16 14:19:35 +0100

    Save unmodified data before adding noise

--- case_studies/test_case_study/scripts/generate_artificial_data.py
+++ case_studies/test_case_study/scripts/generate_artificial_data.py
@@ -13,12 +13,17 @@ sim = Simulation(config=config)
 evaluator = sim.dispatch(theta=sim.model_parameter_dict)
 evaluator()
 
-X = sim.coordinates["time"]
+# save dataset
+evaluator.results.to_netcdf(f"{sim.data_path}/simulated_data.nc")
+
+# add noise to daa
+Y = evaluator.results.copy()
+X = sim.coordinates["time"].copy()
 
 data_index = rng.choice(a=np.arange(len(X)), size=200, replace=False)
 data_index.sort()
 X_stochastic = X[data_index]
-y_noisy = evaluator.results.isel(time=data_index)
+y_noisy = Y.isel(time=data_index)
 
 nan_frac = 0.1
 rng = np.random.default_rng(seed=1)
@@ -31,5 +36,4 @@ for i, (k, val) in enumerate(y_noisy.items()):
 # save noisy dataset
 y_noisy.to_netcdf(f"{sim.data_path}/simulated_noisy_data.nc")
 
-evaluator.results.to_netcdf(f"{sim.data_path}/simulated_data.nc")
 
