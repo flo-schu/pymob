8dcb584fba63cd480bb48a5ecee388ac23595967
flo-schu <fluncki@protonmail.com>
2024-01-23 21:43:03 +0100

    Create a new Simulation and scenario

--- case_studies/test_case_study/sim.py
+++ case_studies/test_case_study/sim.py
@@ -15,10 +15,16 @@ class Simulation(SimulationBase):
             gamma = 0.3,  # Predator reproduction rate
             delta = 0.01,  # Predator death rate
         )
-        self.model_parameters["y0"] = [40, 9]  # initial population of prey and predator
-        self.observations = xr.load_dataset(input[1])
-        self.observations["wolves"] = self.observations["wolves"] + 1e-8
-        self.observations["rabbits"] = self.observations["rabbits"] + 1e-8
+        if self.config.get("simulation", "replicated"):
+            rng = np.random.default_rng(1)
+            self.model_parameters["y0"] = rng.integers(1, 50, size=(10, 2))
+        else:
+            self.model_parameters["y0"] = np.array([40, 9])  # initial population of prey and predator
+
+        
+        # self.observations = xr.load_dataset(input[1])
+        # self.observations["wolves"] = self.observations["wolves"] + 1e-8
+        # self.observations["rabbits"] = self.observations["rabbits"] + 1e-8
 
         
     @staticmethod
@@ -53,3 +59,20 @@ class Simulation(SimulationBase):
     def plot(self, results):
         fig = plot_trajectory(results)
         fig.savefig(f"{self.output_path}/trajectory.png")
+
+# CURRENTLY UNUSABLE SEE https://github.com/flo-schu/pymob/issues/6
+class ReplicatedSimulation(Simulation):
+    def set_coordinates(self, input):
+        if self.observations is not None:
+            return self.observations.time.values
+        
+        # Time settings
+        t_start = 0
+        t_end = 200
+        t_step = 0.1
+        time = np.arange(t_start, t_end, t_step)
+
+        id = np.arange(len(self.model_parameters["y0"]))
+
+
+        return time, id
--- tests/test_numpyro.py
+++ tests/test_numpyro.py
@@ -2,9 +2,9 @@ import numpy as np
 
 from pymob.utils.store_file import prepare_casestudy
 
-def create_simulation():
+def create_simulation(scenario):
     config = prepare_casestudy(
-        case_study=("test_case_study", "test_scenario"),
+        case_study=("test_case_study", scenario),
         config_file="settings.cfg"
     )
     from case_studies.test_case_study.sim import Simulation
@@ -12,7 +12,7 @@ def create_simulation():
 
 def test_diffrax_exception():
     # with proper scripting API define JAX model here or import from fixtures
-    sim = create_simulation()
+    sim = create_simulation("test_scenario")
 
     # diffrax returns infinity for all computed values after which the solver 
     # breaks due to raching maximum number of steps. 
@@ -40,7 +40,33 @@ def test_diffrax_exception():
 
 
 def test_nuts_kernel():
-    sim = create_simulation()
+    sim = create_simulation("test_scenario")
+
+    sim.config.set("inference.numpyro", "kernel", "nuts")
+    sim.set_inferer(backend="numpyro")
+    sim.inferer.run()
+    
+    posterior_mean = sim.inferer.idata.posterior.mean(("chain", "draw"))[sim.model_parameter_names]
+    true_parameters = sim.model_parameter_dict
+    
+    # tests if true parameters are close to recovered parameters from simulated
+    # data
+    np.testing.assert_allclose(
+        posterior_mean.to_dataarray().values,
+        np.array(list(true_parameters.values())),
+        rtol=1e-2, atol=1e-3
+    )
+
+def test_nuts_kernel_replicated():
+    # CURRENTLY UNUSABLE SEE https://github.com/flo-schu/pymob/issues/6
+    config = prepare_casestudy(
+        case_study=("test_case_study", "test_scenario_replicated"),
+        config_file="settings.cfg"
+    )
+    from case_studies.test_case_study.sim import ReplicatedSimulation
+    
+    sim = ReplicatedSimulation(config=config)
+    sim = create_simulation("test_scenario_replicated")
 
     sim.config.set("inference.numpyro", "kernel", "nuts")
     sim.set_inferer(backend="numpyro")
@@ -58,7 +84,7 @@ def test_nuts_kernel():
     )
 
 def test_sa_kernel():
-    sim = create_simulation()
+    sim = create_simulation("test_scenario")
 
     sim.config.set("inference.numpyro", "kernel", "sample-adaptive-mcmc")
     sim.config.set("inference.numpyro", "init_strategy", "init_to_sample")
@@ -93,4 +119,4 @@ if __name__ == "__main__":
     import sys
     import os
     sys.path.append(os.getcwd())
-    test_sa_kernel()
+    test_nuts_kernel_replicated()
