0ec736657aaea5c232f8896279fb8627c51331e5
flo-schu <fluncki@protonmail.com>
2024-08-05 23:34:47 +0200

    test guts case studies

--- tests/test_case_studies.py
+++ tests/test_case_studies.py
@@ -2,6 +2,7 @@ import numpy as np
 from tests.fixtures import (
     init_guts_casestudy_constant_exposure,
     init_guts_casestudy_variable_exposure,
+    init_bufferguts_casestudy,
 )
 
 # import jax with 64 bit precision
@@ -38,7 +39,7 @@ def test_constant_exposure():
 def test_variable_exposure():
     sim = init_guts_casestudy_variable_exposure()
 
-    sim.use_symbolic_solver()
+    sim.use_symbolic_solver(do_compile=True)
     evaluator = sim.dispatch(theta={})
     evaluator()
     sol_symbolic = evaluator.results
@@ -64,6 +65,33 @@ def test_variable_exposure():
     axes = sim._plot.plot_multiexposure(sol_symbolic, vars=["exposure", "D", "H", "S"], axes=axes, color="tab:red", linestyle="--", label_prefix="exact")
     fig = axes[0].figure
     fig.savefig(f"{sim.output_path}/solution_comparison.png")
+
+
+def test_variable_exposure_compiled():
+    sim = init_guts_casestudy_variable_exposure()
+
+    sim.use_symbolic_solver(do_compile=False)
+    evaluator = sim.dispatch(theta={})
+    evaluator()
+    sol_symbolic = evaluator.results
+
+    sim.use_jax_solver()
+    sim.dispatch_constructor(rtol=1e-15, atol=1e-16)
+    evaluator = sim.dispatch(theta={})
+    evaluator()
+    sol_numerical = evaluator.results
+
+    # make sure errors are small between exact solution and numerical solution
+    # the errors come from:
+    # a) integrating not exactly to t_eq
+    # b) using a numerical switch in the ODE solution.
+    diff = (
+        sol_numerical.sel(time=np.arange(0,sim.t_max))
+        - sol_symbolic.sel(time=np.arange(0,sim.t_max))
+    )[["D", "H", "S"]]
+    max_delta = np.abs(diff).max().to_array()
+    np.testing.assert_array_less(max_delta, [1e-8, 1e-8, 1e-8])
+
 
 
 
