6c175ef1d1a096395675bd78305bad19f9e2d098
flo-schu <fluncki@protonmail.com>
2024-08-06 14:18:11 +0200

    Add test for importing different case studies and reduce precision of jax solver

--- tests/test_case_studies.py
+++ tests/test_case_studies.py
@@ -1,20 +1,26 @@
 import numpy as np
 import pytest
 from tests.fixtures import (
+    init_simulation_casestudy_api,
     init_guts_casestudy_constant_exposure,
     init_guts_casestudy_variable_exposure,
     init_bufferguts_casestudy,
 )
 
-# import jax with 64 bit precision
-import jax
-jax.config.update("jax_enable_x64", True)
+
+def test_multiple_case_study_import():
+    sim = init_simulation_casestudy_api()
+    sim = init_guts_casestudy_constant_exposure()
+    sim = init_bufferguts_casestudy()
+    sim = init_bufferguts_casestudy()
+    sim = init_simulation_casestudy_api()
+
 
 def test_guts_constant_exposure():
     sim = init_guts_casestudy_constant_exposure()
 
     sim.use_jax_solver()
-    sim.dispatch_constructor(rtol=1e-15, atol=1e-16)
+    sim.dispatch_constructor(rtol=1e-7, atol=1e-8)
     evaluator = sim.dispatch(theta={})
     evaluator()
     
@@ -33,7 +39,7 @@ def test_guts_constant_exposure():
     )
 
     max_delta = np.abs(diff).max().to_array()
-    np.testing.assert_array_less(max_delta, [1e-8, 1e-8, 1e-8])
+    np.testing.assert_array_less(max_delta, [1e-2, 1e-2, 1e-2])
 
     
 
@@ -46,7 +52,7 @@ def test_guts_variable_exposure():
     sol_symbolic = evaluator.results
 
     sim.use_jax_solver()
-    sim.dispatch_constructor(rtol=1e-15, atol=1e-16)
+    sim.dispatch_constructor(rtol=1e-7, atol=1e-8)
     evaluator = sim.dispatch(theta={})
     evaluator()
     sol_numerical = evaluator.results
@@ -60,7 +66,7 @@ def test_guts_variable_exposure():
         - sol_symbolic.sel(time=np.arange(0,sim.t_max))
     )[["D", "H", "S"]]
     max_delta = np.abs(diff).max().to_array()
-    np.testing.assert_array_less(max_delta, [1e-8, 1e-8, 1e-8])
+    np.testing.assert_array_less(max_delta, [5e-2, 5e-2, 5e-2])
 
     axes = sim._plot.plot_multiexposure(sol_numerical, vars=["exposure", "D", "H", "S"], color="tab:blue", label_prefix="ODE")
     axes = sim._plot.plot_multiexposure(sol_symbolic, vars=["exposure", "D", "H", "S"], axes=axes, color="tab:red", linestyle="--", label_prefix="exact")
@@ -77,7 +83,7 @@ def test_guts_variable_exposure_compiled():
     sol_symbolic = evaluator.results
 
     sim.use_jax_solver()
-    sim.dispatch_constructor(rtol=1e-15, atol=1e-16)
+    sim.dispatch_constructor(rtol=1e-7, atol=1e-8)
     evaluator = sim.dispatch(theta={})
     evaluator()
     sol_numerical = evaluator.results
@@ -91,7 +97,7 @@ def test_guts_variable_exposure_compiled():
         - sol_symbolic.sel(time=np.arange(0,sim.t_max))
     )[["D", "H", "S"]]
     max_delta = np.abs(diff).max().to_array()
-    np.testing.assert_array_less(max_delta, [1e-8, 1e-8, 1e-8])
+    np.testing.assert_array_less(max_delta, [5e-2, 5e-2, 5e-2])
 
 
 
@@ -110,7 +116,7 @@ def test_bufferguts_variable_exposure():
     sol_discrete = evaluator.results
 
     sim.use_jax_solver()
-    sim.dispatch_constructor(rtol=1e-15, atol=1e-16, max_steps=1e6)
+    sim.dispatch_constructor(rtol=1e-7, atol=1e-8, max_steps=1e6)
     evaluator = sim.dispatch(theta={})
     evaluator()
     sol_numerical = evaluator.results
@@ -143,7 +149,7 @@ def test_bufferguts_hybrid_solution():
     sol_symbolic = evaluator.results
 
     sim.use_jax_solver()
-    sim.dispatch_constructor(rtol=1e-15, atol=1e-16, max_steps=1e6)
+    sim.dispatch_constructor(rtol=1e-7, atol=1e-8, max_steps=1e6)
     evaluator = sim.dispatch(theta={})
     evaluator()
     sol_numerical = evaluator.results
@@ -157,7 +163,9 @@ def test_bufferguts_hybrid_solution():
         - sol_symbolic.sel(time=np.arange(0,sim.t_max))
     )[["B", "D", "H", "S"]]
     max_delta = np.abs(diff).max().to_array()
-    np.testing.assert_array_less(max_delta, [1e-1, 1e-1, 1e-1, 1e-1])
+    # large errors are partially due to the solver running at 64 bit precision 
+    # (exact to 8 digits)
+    np.testing.assert_array_less(max_delta, [0.25, 0.25, 0.25, 0.25])
 
 
     axes = sim._plot.plot_multiexposure(sol_numerical, vars=["exposure", "B", "D", "H", "S"], color="tab:blue", label_prefix="ODE")
@@ -167,7 +175,11 @@ def test_bufferguts_hybrid_solution():
 
 
 if __name__ == "__main__":
+    import sys
+    import os
+    sys.path.append(os.getcwd())
     pass
+    # test_multiple_case_study_import()
     # test_constant_exposure()
     # test_variable_exposure()
     # test_bufferguts_variable_exposure()
