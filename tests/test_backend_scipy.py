aee155a745c2fdabfe8e39b795801e22d516ff96
flo-schu <fluncki@protonmail.com>
2024-08-16 12:05:07 +0200

    Increase EPS in observatiosn and simulation results to get rid of nan likelihoods

--- tests/test_backend_scipy.py
+++ tests/test_backend_scipy.py
@@ -91,6 +91,52 @@ def test_parameter_parsing_different_priors_on_species():
         [alpha_numpyro_cottontail, alpha_numpyro_jackrabbit], [1, 3], decimal=1
     )
 
+    try:
+        sim.inferer.run()
+        raise AssertionError(
+            "This model should fail, because there are negative values in the"+
+            "observations, hence the log-likelihood becomes nan, because there"+
+            "is no support for the values"
+        )
+    except RuntimeError:
+        # check likelihoods of rabbits     
+        loglik = sim.inferer.check_log_likelihood(theta)
+        nan_liks = np.isnan(loglik[2]["rabbits_obs"]).sum()
+        assert nan_liks > 0
+
+    # The conclusion is that this cannot work, because the simulation produces
+    # values that are below zero frequently. I need to increase EPS to work
+    # sim.config.jaxsolver.throw_exception
+    # sim.config.jaxsolver.max_steps = 100000
+    # sim.config.inference_numpyro.init_strategy = "init_to_sample"
+
+    # we need to set both observations to values greater zero, because
+    # the lognormal distribution has no support for values equal to zero.
+    sim.observations = e.results.round() + 1e-6
+    if np.any(sim.observations.min().to_array().values < 0):
+        raise AssertionError(
+            "observations had values < 0. This causes NaN log-likelihoods."
+        )
+
+
+    # we need to set both observations to values greater zero, because
+    # the lognormal distribution has no support for values equal to zero.
+    # in the inference backend, values returned from the simulator are modified
+    sim.config.error_model.rabbits = "lognorm(scale=rabbits+1e-6, s=0.1)"
+    sim.config.error_model.rabbits = "lognorm(scale=rabbits+EPS, s=0.1)"
+    sim.config.inference.eps = 1e-6
+    sim.dispatch_constructor()
+    sim.set_inferer("numpyro")
+
+    idata = sim.inferer.prior_predictions()
+    idata.prior_predictive.rabbits.min()
+
+    loglik = sim.inferer.check_log_likelihood(theta)
+    nan_liks_rabbits = np.isnan(loglik[2]["rabbits_obs"]).sum()
+    nan_liks_wolves = np.isnan(loglik[2]["wolves_obs"]).sum()
+
+    np.testing.assert_array_equal([nan_liks_wolves, nan_liks_rabbits], [0,0])
+
 
 def test_parameter_parsing_different_priors_on_year():
     sim = init_test_case_study_hierarchical()
