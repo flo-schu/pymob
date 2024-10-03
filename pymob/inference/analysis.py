b321c8b2536849bc4da2c07aecda53b50806e7db
flo-schu <fluncki@protonmail.com>
2024-10-03 22:20:27 +0200

    Implement prior likelihood plot

--- pymob/inference/analysis.py
+++ pymob/inference/analysis.py
@@ -415,44 +415,79 @@ def evaluate_posterior(sim, nesting_dimension, n_samples=10_000, vars_table={},
 
 
 
-def plot_pairs(posterior, likelihood):
-    parameters = list(posterior.data_vars.keys())
+def plot_pair(posterior, likelihood, parameters, axes=None):
 
-    N = len(parameters)
-    parameters_ = parameters.copy()
-    fig = plt.figure(figsize=(3*N, 3*(N+1)))
-    gs = fig.add_gridspec(N, N+1, width_ratios=[1]*N+[0.2])
+    likelihood_vars = list(likelihood.data_vars.keys())
+    N = len(likelihood_vars)
+
+    if axes is None:
+        fig, axes = plt.subplots(1, N, figsize=(3+(2*(N-1)), 3), sharey=True, sharex=True)
+    
+    if isinstance(likelihood, xr.DataArray):
+        if likelihood.name is None:
+            likelihood.name = "joint_likelihood"
+        likelihood = likelihood.to_dataset()
     
 
-    i = 0
-    while len(parameters_) > 0:
-        par_x = parameters_.pop(0)
-        hist_ax = gs[i,i].subgridspec(1, 1).subplots()
-        plot_hist(
-            posterior[par_x].stack(sample=("chain", "draw")), 
-            ax=hist_ax, decorate=False, bins=20
-        )
-        hist_ax.set_title(par_x)
-        for j, par_y in enumerate(parameters_, start=i+1):
-            ax = gs[j,i].subgridspec(1, 1).subplots()
+    par_a, par_b = parameters
 
-            scatter = ax.scatter(
-                posterior[par_x], 
-                posterior[par_y], 
-                c=likelihood, 
-                alpha=0.25,
-                s=10,
-                cmap=mpl.colormaps["plasma_r"]
-            )
+    post_a = posterior[par_a]
+    post_b = posterior[par_b]
 
-            if j != len(parameters)-1:
-                ax.set_xticks([])
+    # always put the smaller parameter on the x-axis
+    if post_a.ndim <= post_b.ndim:
+        post_x = post_a
+        post_y = post_b
+    else:
+        post_x = post_b.stack(sample=("chain", "draw"))
+        post_y = post_a.stack(sample=("chain", "draw"))
+
+    loglik = likelihood.stack(sample=("chain","draw"))
+    extra_dims_y = [d for d in post_y.dims if d not in post_x.dims]
+    norm = mpl.colors.Normalize(vmin=(likelihood).to_array().min(), vmax=(likelihood).to_array().max())
+    cmap = mpl.colormaps["plasma"]
+
+    for likvar, ax in zip(likelihood_vars, axes):
+        ll = loglik[likvar]
+        ax.set_title(likvar)
+        ax.set_xlabel(post_x.name)
+        ax.set_ylabel(post_y.name)
+        for dim in extra_dims_y:
+            for coord in post_y.coords[dim].values:
+                if dim in ll.dims:
+                    ll_coord = ll.sel({dim:coord})
+                else:
+                    ll_coord = ll
+
+                scatter = ax.scatter(
+                    post_x, 
+                    post_y.sel({dim:coord}), 
+                    c=ll_coord, 
+                    alpha=0.25,
+                    marker=f"${coord}$",
+                    s=40,
+                    cmap=cmap,
+                    norm=norm
+                )
+
+                # ax.annotate()
         
-            ax.set_xlabel(par_x)            
-            ax.set_ylabel(par_y)
+    cbar = fig.colorbar(
+        mappable=mpl.cm.ScalarMappable(norm=norm, cmap=cmap), 
+        # if the same axis is used it is plot next to
+        # the plot depending on the location option
+        # ax=ax,
+        # location="right",
+        orientation="vertical",
 
-        i += 1
+        # if another axis is used, the fraction should be increased to 1.0
+        ax=ax,
+        fraction=0.1,
+        pad=0.05,
+        # aspect=20, # parameter is not so important. It thins the colorbar
+        label="negative log-likelihood"
+    )
+    fig.tight_layout()
 
     # ax_colorbar = gs[:,N].subgridspec(1, 1).subplots()
-    # fig.colorbar(scatter, cax=ax_colorbar)
     return fig
