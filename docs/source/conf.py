aaea132969cd27c981e1fca3b6f1c416f8f4e509
flo-schu <fluncki@protonmail.com>
2025-01-08 21:26:13 +0100

    bump version 0.5.0a10 -> 0.5.0a11

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "pymob"
 copyright = "2024, Florian Schunck"
 author = "Florian Schunck"
-release = "0.5.0a10"
+release = "0.5.0a11"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
