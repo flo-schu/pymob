7ff8faa2310397f849d7063cd5770add7a6c1d8c
flo-schu <fluncki@protonmail.com>
2025-03-02 23:01:39 +0100

    bump version 0.5.1 -> 0.5.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "pymob"
 copyright = "2024, Florian Schunck"
 author = "Florian Schunck"
-release = "0.5.1"
+release = "0.5.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- pymob/__init__.py
+++ pymob/__init__.py
@@ -4,7 +4,7 @@ from . import utils
 from . import solvers
 from . import examples
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 
 from .simulation import SimulationBase
