4b71e0bfe466c247e49be52efc166615f09976a5
flo-schu <fluncki@protonmail.com>
2025-03-03 00:49:58 +0100

    Update case studies examples in docs

--- docs/run_example_case_studies.sh
+++ docs/run_example_case_studies.sh
@@ -10,4 +10,4 @@ else
 fi
 
 jupyter nbconvert --to markdown ${nb_exec} case_studies/lotka_volterra_case_study/scripts/*.ipynb --output-dir=docs/source/examples/lotka_volterra_case_study/
-jupyter nbconvert --to markdown ${nb_exec} case_studies/tktd_rna_pulse/scripts/*.ipynb --output-dir=docs/source/examples/tktd_rna_pulse/
+# jupyter nbconvert --to markdown ${nb_exec} case_studies/tktd_rna_pulse/scripts/*.ipynb --output-dir=docs/source/examples/tktd_rna_pulse/
