aaf116821f0b6e44e4f735b2049905543916df1a
flo-schu <fluncki@protonmail.com>
2025-06-04 23:05:40 +0200

    normilze additional path in testfile

--- tests/test_config.py
+++ tests/test_config.py
@@ -82,8 +82,8 @@ def test_load_generated_settings():
     assert sim.config.case_study.data == None
     assert normalize_path(sim.config.case_study.data_path) == normalize_path("case_studies\\lotka_volterra_case_study\\data")
     assert sim.config.case_study.output == None
-    assert sim.config.case_study.output_path == \
-        "case_studies/lotka_volterra_case_study/results/test_scenario_scripting_api"
+    assert normalize_path(sim.config.case_study.output_path) == \
+        normalize_path("case_studies/lotka_volterra_case_study/results/test_scenario_scripting_api")
 
 def test_load_interpolated_settings():
     sim = SimulationBase(f"{scenario}/interp_settings.cfg")
