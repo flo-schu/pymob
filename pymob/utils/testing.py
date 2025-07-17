60b7dbf8ec6a91c8ed32b8a8434618144c54625d
flo-schu <fluncki@protonmail.com>
2025-07-17 16:45:04 +0200

    Implement and test import export and copy methods

--- pymob/utils/testing.py
+++ pymob/utils/testing.py
@@ -10,3 +10,35 @@ def assert_no_nans_in_dataset(dataset):
     if nans:
         raise ValueError("Dataset contained NaN values")
 
+def _compare_config(config_a, config_b, ignore_fields={}):
+    sections_mismatch = {}
+    for section in config_a.model_fields.keys():
+        a_section = getattr(config_a, section)
+        b_section = getattr(config_b, section)
+
+
+        if a_section != b_section:
+            fields_mismatch = {}
+
+            for field in a_section.model_fields_set:
+                if field in ignore_fields.get(section, []):
+                    continue
+
+                a_field = getattr(a_section, field)
+                b_field = getattr(b_section, field, "NOT DEFINED")
+
+                if a_field != b_field:
+                    fields_mismatch.update(
+                        {field: {"origin": a_field, "export": b_field}}
+                    )
+
+            if len(fields_mismatch) > 0:
+                sections_mismatch.update({section:fields_mismatch})
+
+        else:
+            pass
+
+    if len(sections_mismatch) > 0:
+        raise AssertionError(
+            f"Config files do not match: {sections_mismatch=}"
+        )
