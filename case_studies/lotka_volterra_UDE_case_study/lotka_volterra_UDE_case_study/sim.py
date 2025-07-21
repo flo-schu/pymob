9ad885c421041e5942bcd38d84aa5bc9fc3936c6
merkuns <mholdreich@uni-osnabrueck.de>
2025-07-21 11:35:07 +0200

    fixed imports

--- case_studies/lotka_volterra_UDE_case_study/lotka_volterra_UDE_case_study/sim.py
+++ case_studies/lotka_volterra_UDE_case_study/lotka_volterra_UDE_case_study/sim.py
@@ -1,5 +1,5 @@
 from case_studies.lotka_volterra_case_study.lotka_volterra_case_study.sim import Simulation_v2
-from case_studies.lotka_volterra_UDE_case_study.lotka_volterra_UDE_case_study.mod import lotka_volterra_UDE, Func, transformWeightsBackwards, transformBiasBackwards, setFuncWeightsAndBias
+from case_studies.lotka_volterra_UDE_case_study.lotka_volterra_UDE_case_study.mod import Func
 from pymob.solvers.diffrax import JaxSolver
 import xarray as xr
 import numpy as np
@@ -14,7 +14,7 @@ class UDESimulation(Simulation_v2):
 
     key = jr.PRNGKey(5678)
     data_key, model_key, loader_key = jr.split(key, 3)
-    model = Func(10,10,key=model_key,theta_true=jnp.array([alpha,beta,gamma]))
+    model = Func(10,10,key=model_key,theta_true=(alpha,beta,gamma))
     
     solver = JaxSolver
 
@@ -27,7 +27,7 @@ class UDESimulation(Simulation_v2):
 
         key = jr.PRNGKey(5678)
         data_key, model_key, loader_key = jr.split(key, 3)
-        self.model = Func(10,10,key=model_key,theta_true=jnp.array([alpha,beta,gamma]))
+        self.model = Func(10,10,key=model_key,theta_true=(alpha,beta,gamma))
 
         self.observations = xr.load_dataset(input[0])
 
@@ -37,27 +37,27 @@ class UDESimulation(Simulation_v2):
         self.model_parameters["parameters"] = self.config.model_parameters.value_dict
 
 
-class UDESimulation2(Simulation_v2):
-    solver = JaxSolver
+# class UDESimulation2(Simulation_v2):
+#     solver = JaxSolver
 
-    def initialize(self, input):
-        self.observations = xr.load_dataset(input[0])
+#     def initialize(self, input):
+#         self.observations = xr.load_dataset(input[0])
 
-        y0 = self.parse_input("y0", drop_dims=["time"])
-        self.model_parameters["y0"] = y0
+#         y0 = self.parse_input("y0", drop_dims=["time"])
+#         self.model_parameters["y0"] = y0
 
-        self.model_parameters["parameters"] = self.config.model_parameters.value_dict
+#         self.model_parameters["parameters"] = self.config.model_parameters.value_dict
 
-        weights = returnWeightsList(self, 70)
-        bias = returnBiasList(self, 17)
+#         weights = returnWeightsList(self, 70)
+#         bias = returnBiasList(self, 17)
 
-        key = jr.PRNGKey(5678)
-        data_key, model_key, loader_key = jr.split(key, 3)
+#         key = jr.PRNGKey(5678)
+#         data_key, model_key, loader_key = jr.split(key, 3)
 
-        func = Func(2,5,3,key=model_key,theta_true=jnp.array([self.model_parameters["parameters"]["alpha"],self.model_parameters["parameters"]["gamma"]]))
-        func = setFuncWeightsAndBias(func, weights, bias, key=model_key)
+#         func = Func(2,5,3,key=model_key,theta_true=jnp.array([self.model_parameters["parameters"]["alpha"],self.model_parameters["parameters"]["gamma"]]))
+#         func = setFuncWeightsAndBias(func, weights, bias, key=model_key)
 
-        self.model = func
+#         self.model = func
 
 
 def returnWeightsList(self, noWeights):
