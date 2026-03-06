dd13fbf00005729b1b87fc0ff003f27470906f61
flo-schu <fluncki@protonmail.com>
2026-03-06 12:35:21 +0100

    Add new config structure to API documentation by defining public methods with __all__ in pymob.sim.config.__init__

--- pymob/sim/config/__init__.py
+++ pymob/sim/config/__init__.py
@@ -10,3 +10,27 @@ from .parameters import *
 from .base import *
 
 from .casestudy_registry import *
+
+__all__ = [
+    "Config",
+    "PymobModel",
+    "Casestudy",
+    "Simulation",
+    "Datastructure",
+    "DataVariable",
+    "Modelparameters",
+    "Expression",
+    "RandomVariable",
+    "Param",
+    "Errormodel",
+    "Solverbase",
+    "Jaxsolver",
+    "Multiprocessing",
+    "Inference",
+    "Numpyro",
+    "Pyabc",
+    "Redis",
+    "Pymoo",
+    "Report",
+    "register_case_study_config",
+]
